2d7d78289c926454d60ffc512af17310e400af4e
ekaterina <grig.katrin@gmail.com>
2024-11-19 12:44:10 +0200

    adding summary methods for readme file

--- tools/hf_search.py
+++ tools/hf_search.py
@@ -1,6 +1,9 @@
+import os
 import re
 
+import openai
 import requests
+import tiktoken
 from bs4 import BeautifulSoup
 
 class HuggingFaceSearch:
@@ -89,4 +92,70 @@ class HuggingFaceSearch:
             timestamp = model.find('time').attrs['datetime']
             yield {"model_name": model_name, "last_updated": timestamp, "liked": liked}
 
+    @staticmethod
+    def summarize_one_chunk(chunk):
+        """
+        Summarize one portion of text from the long page.
+        :param chunk: Text portion that will be summarized
+        :return: Summary of the chunk
+        """
+        try:
+            client = openai.OpenAI(
+                api_key=os.getenv("OPENAI_API_KEY")
+            )
+            messages_for_model = [
+                {"role": "system",
+                 "content": "You're a researcher who analyse the files and gives a summary with most relevant info"},
+                {"role": "user", "content": f"Give short summary the following README content:\n\n{chunk}"}]
 
+            response = client.chat.completions.create(
+                model="gpt-4o-mini",
+                messages=messages_for_model,
+                temperature=0.5,
+            )
+            return response.choices[0].message.content
+        except requests.exceptions.RequestException as e:
+            print(f"Error fetching README: {e}")
+            return None
+
+    @staticmethod
+    def split_text_by_tokens(long_text, chunk_size=10000):
+        """Splits a long text into portions with a maximum number of tokens.
+        :param long_text: Long text to split
+        :param chunk_size: Number of tokens in each portion.
+        Returns:A list of text portions.
+        """
+
+        tokenizer = tiktoken.encoding_for_model("gpt-4o-mini")
+        tokens = tokenizer.encode(long_text)
+
+        chunks = [tokens[i:i + chunk_size] for i in range(0, len(tokens), chunk_size)]
+        decoded_chunks = [tokenizer.decode(chunk) for chunk in chunks]
+
+        return decoded_chunks
+
+    def summarize_page(self, page_url):
+        """
+        Summarize a page of text, originally developed for README files on huggingface.co
+        :param page_url: Link to the page that needs to be summarized
+        :return: Full summary of the page
+        """
+        try:
+            response = requests.get(page_url)
+            response.raise_for_status()  # Raise an exception for bad status codes
+            soup = BeautifulSoup(response.content.decode('utf8'))
+            readme_content = soup.get_text(separator='\n').replace('\n', ' ').replace('\t', ' ').strip()
+            readme_content = ' '.join(readme_content.split())
+
+            tokenizer = tiktoken.encoding_for_model("gpt-4o-mini")
+            tokens = tokenizer.encode(readme_content)
+
+            if len(tokens) > 128000:
+                readme_content = self.split_text_by_tokens(readme_content)
+                summaries = [self.summarize_one_chunk(chunk) for chunk in readme_content]
+                return ' '.join(summaries)
+
+            return self.summarize_one_chunk(readme_content)
+        except requests.exceptions.RequestException as e:
+            print(f"Error fetching README: {e}")
+            return None
