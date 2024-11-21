8d0c0f602b1cdc32a00ab6f959aca449f7c41dfe
Алексей Степин <alekseystepin13@gmail.com>
2024-11-21 11:06:06 +0200

    docker for integration fixes

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
+import time
+import os
 from runner import get_res
 
-
 while True:
-    # Here we search for files in volume from architects
-    # If find one, Parse their file as PROMPT
-    # Save_dit = Path to volume + research_<current_task_id>
-    # Current task id???
-    PROMPT = "Some Prompt"
-    save_dir = "volume/research_1.txt"
-    id_ = "1"
-    get_res(PROMPT, save_dir, id_)
+
+    VOLUME = "tmp/jass"
+    for filename in os.listdir(VOLUME):
+        if filename.startswith("research_result_") and filename.endswith(".txt"):
+            task_id = filename[len("research_result_"):-len(".txt")]
+            file_path = os.path.join(VOLUME, filename)
+            with open(file_path, 'r') as file:
+                prompt_content = file.read()
+            get_res(prompt_content, file_path, task_id)
+    time.sleep(30)
