b75b2a926e9030f0e67c7fe938078f154ec975ee
Алексей Степин <alekseystepin13@gmail.com>
2024-11-21 11:29:52 +0200

    fixed app.py

--- app.py
+++ app.py
@@ -2,14 +2,20 @@ import time
 import os
 from runner import get_res
 
-while True:
+VOLUME = os.getenv("VOLUME", "tmp/jass")
+processed_ids = set()
 
-    VOLUME = "tmp/jass"
+while True:
     for filename in os.listdir(VOLUME):
-        if filename.startswith("research_result_") and filename.endswith(".txt"):
-            task_id = filename[len("research_result_"):-len(".txt")]
+        if filename.startswith("research_task_") and filename.endswith(".txt"):
+            task_id = filename[len("research_task_"):-len(".txt")]
+            if task_id in processed_ids:
+                continue
             file_path = os.path.join(VOLUME, filename)
             with open(file_path, 'r') as file:
                 prompt_content = file.read()
-            get_res(prompt_content, file_path, task_id)
+            result_filename = f"research_result_{task_id}.txt"
+            result_path = os.path.join(VOLUME, result_filename)
+            get_res(prompt_content, result_path, task_id)
+            processed_ids.add(task_id)
     time.sleep(30)
